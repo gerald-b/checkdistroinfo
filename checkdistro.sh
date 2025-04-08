bcf3facbba3f5769a9b7eb30be50619ca5f93989
Gerald Büchler <dev@genomast.de>
2025-04-08 16:32:43 +0200

    change: refactoring to functions

--- checkdistro.sh
+++ checkdistro.sh
@@ -2,79 +2,119 @@
 
 TMD=/tmp/distro.md
 
-echo -e "# DISTRO Information" > ${TMD}
-echo -e "## Kernel Version" >> ${TMD}
-echo -e "\`\`\`" >> ${TMD}
-uname -r >> ${TMD}
-echo -e "\`\`\`" >> ${TMD}
+main()
+{
+	echo -e "# DISTRO Information" > ${TMD}
+	kernelinfo
+	lsbinfo
+	packageinfo
+	programminfo
+	listflatpak
+	listsnap
+	enviroment_vars
+	echo -e "# EOF" >> ${TMD}
+}
 
-echo -e "## LSB Release info" >> ${TMD}
-echo -e "\`\`\`" >> ${TMD}
-lsb_release -a 2>/dev/null >> ${TMD}
-echo -e "\`\`\`" >> ${TMD}
-
-echo -e "## Package Information" >> ${TMD}
-if [ "$(whereis apt | grep -i /bin/apt | wc -l)" -eq "1" ]; then
+kernelinfo()
+{
+	echo -e "## Kernel Version" >> ${TMD}
 	echo -e "\`\`\`" >> ${TMD}
-	echo -e "Packagemanager:\tapt" >> ${TMD}
-	echo -ne "Packagecount:\t" >> ${TMD}
-	apt list --installed | wc -l 2>/dev/null >> ${TMD}
+	uname -r >> ${TMD}
 	echo -e "\`\`\`" >> ${TMD}
-fi
-# arch / pacman
-# pacman -Qq | wc -l
+}
 
-# Fedora
-# dnf list installed | wc -l
+lsbinfo()
+{
+	echo -e "## LSB Release info" >> ${TMD}
+	echo -e "\`\`\`" >> ${TMD}
+	lsb_release -a 2>/dev/null >> ${TMD}
+	echo -e "\`\`\`" >> ${TMD}
+}
+
+packageinfo()
+{
+	echo -e "## Package Information" >> ${TMD}
+	if [ "$(whereis apt | grep -i /bin/apt | wc -l)" -eq "1" ]; then
+		echo -e "\`\`\`" >> ${TMD}
+		echo -e "Packagemanager:\tapt" >> ${TMD}
+		echo -ne "Packagecount:\t" >> ${TMD}
+		apt list --installed | wc -l 2>/dev/null >> ${TMD}
+		echo -e "\`\`\`" >> ${TMD}
+	fi
+
+	# arch / pacman
+	# pacman -Qq | wc -l
+	
+	# Fedora
+	# dnf list installed | wc -l
+}
+
+programminfo()
+{
+	echo -e "## Programm Information" >> ${TMD}
+	echo -e "|program|state|" >> ${TMD}
+	echo -e "| ------------- |:-------------:|" >> ${TMD}
+	
+	search4pgm=pipewire
+	programmdetails
+	search4pgm=flatpak
+	programmdetails
+	search4pgm=snap
+	programmdetails
+
+}
+
+programmdetails()
+{
+	echo -ne "| ${search4pgm} |" >> ${TMD}
+	if [ "$(whereis ${search4pgm} | grep -i /bin/${search4pgm} | wc -l)" -eq "1" ]; then
+		echo -ne "**installed**" >> ${TMD}
+	else
+		echo -ne "missing" >> ${TMD}
+	fi
+	echo -e "|" >> ${TMD}
+}
+
+listflatpak()
+{
+	if [ "$(whereis flatpak | grep -i /bin/flatpak | wc -l)" -eq "1" ]; then
+		echo -e "## Flatpak installed packages" >> ${TMD}
+	fi
+}
+
+listsnap()
+{
+	if [ "$(whereis snap | grep -i /bin/snap | wc -l)" -eq "1" ]; then
+		echo -e "## Snap installed packages" >> ${TMD}
+		echo -e "\`\`\`" >> ${TMD}
+		snap list >> ${TMD}
+		echo -e "\`\`\`" >> ${TMD}
+	fi
+}
+
+enviroment_vars()
+{
+	echo -e "## Enviroment" >> ${TMD}
+	echo -e "|varialbe            | value               |" >> ${TMD}
+	echo -e "| ------------------ |:-------------------:|" >> ${TMD}
+	echo -e "| \$XDG_SESSION_TYPE | ${XDG_SESSION_TYPE} |" >> ${TMD}
+	echo -e "| \$SHELL            | ${SHELL}            |" >> ${TMD}
+	echo -e "\n" >> ${TMD}
+	echo -e "<details>" >> ${TMD}
+	echo -e "  <summary>Show full enviroment variable list</summary>" >> ${TMD}
+	echo -ne "\n" >> ${TMD}
+	echo -e "\`\`\`" >> ${TMD}
+	printenv >> ${TMD}
+	echo -e "\`\`\`" >> ${TMD}
+	echo -e "</details>\n" >> ${TMD}
+}
+
+main
+
+exit 0
 
 
-echo -e "## Programm Information" >> ${TMD}
-echo -e "|program|state|" >> ${TMD}
-echo -e "| ------------- |:-------------:|" >> ${TMD}
-
-echo -ne "| pipewire |" >> ${TMD}
-if [ "$(whereis pipewire | grep -i /bin/pipewire | wc -l)" -eq "1" ]; then
-	echo -ne "**installed**" >> ${TMD}
-else
-	echo -ne "missing" >> ${TMD}
-fi
-echo -e "|" >> ${TMD}
-
-echo -ne "| flatpak |" >> ${TMD}
-if [ "$(whereis flatpak | grep -i /bin/flatpak | wc -l)" -eq "1" ]; then
-	echo -ne "**installed**" >> ${TMD}
-else
-	echo -ne "missing" >> ${TMD}
-fi
-echo -e "|" >> ${TMD}
-
-echo -ne "| snap |" >> ${TMD}
-if [ "$(whereis snap | grep -i /bin/snap | wc -l)" -eq "1" ]; then
-	echo -ne "**installed**" >> ${TMD}
-else
-	echo -ne "missing" >> ${TMD}
-fi
-echo -e "|" >> ${TMD}
-
-# flatpak list
-
-
-
-echo -e "## Enviroment" >> ${TMD}
-echo -e "|varialbe            | value               |" >> ${TMD}
-echo -e "| ------------------ |:-------------------:|" >> ${TMD}
-echo -e "| \$XDG_SESSION_TYPE | ${XDG_SESSION_TYPE} |" >> ${TMD}
-echo -e "| \$SHELL            | ${SHELL}            |" >> ${TMD}
-
-
-echo -e "<details>" >> ${TMD}
-echo -e "  <summary>Show full enviroment variable list</summary>" >> ${TMD}
-echo -ne "\n" >> ${TMD}
-echo -e "\`\`\`" >> ${TMD}
-printenv >> ${TMD}
-echo -e "\`\`\`" >> ${TMD}
-echo -e "</details>\n" >> ${TMD}
-
+##### TODO
 
 ## FSTYPE
 #lsblk -fo NAME,FSTYPE,UUID
@@ -98,4 +138,3 @@ echo -e "</details>\n" >> ${TMD}
 #echo $(grep -i "swaptotal" /proc/meminfo | cut -d':' -f2 | grep -o '[^[:space:]].*[^[:space:]]' | cut -d' ' -f1) | awk '{printf "%20.2f GB\n", ($1/1024/1024)}'
 
 
-echo -e "# EOF" >> ${TMD}
