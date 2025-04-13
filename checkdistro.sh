a9dac49a8b16742f02508d3f28ae6ae98a16cc6c
Gerald Büchler <dev@genomast.de>
2025-04-13 21:56:19 +0200

    changed: use spaces instead of tabs

--- checkdistro.sh
+++ checkdistro.sh
@@ -10,19 +10,19 @@ main()
     rm ${TMD}
     pandoc_head
     echo -e "# DISTRO Information (Hostname: $(uname -n))" >> ${TMD}
-	kernelinfo
-	lsbinfo
-	packageinfo
-	programminfo
-	listflatpak
-	listsnap
-	enviroment_vars
+    kernelinfo
+    lsbinfo
+    packageinfo
+    programminfo
+    listflatpak
+    listsnap
+    enviroment_vars
     filesystem_info
     cpuinfo
 
     pandoc_pre
     pandoc_export
-	echo -e "# EOF" >> ${TMD}
+    echo -e "# EOF" >> ${TMD}
 }
 
 pandoc_head()
@@ -39,15 +39,15 @@ pandoc_head()
 
 kernelinfo()
 {
-	echo -e "## Kernel Version" >> ${TMD}
-	echo -e "\`\`\`" >> ${TMD}
-	uname -r >> ${TMD}
-	echo -e "\`\`\`" >> ${TMD}
+    echo -e "## Kernel Version" >> ${TMD}
+    echo -e "\`\`\`" >> ${TMD}
+    uname -r >> ${TMD}
+    echo -e "\`\`\`" >> ${TMD}
 }
 
 lsbinfo()
 {
-	echo -e "## LSB Release info" >> ${TMD}
+    echo -e "## LSB Release info" >> ${TMD}
     echo -e "|Description|Value|" >> ${TMD}
     echo -e "|-------------|-------------|" >> ${TMD}
     lsb_release -a 2>/dev/null | sed -e 's/:\t/|/g' -e 's/^/|/' -e 's/$/|/' >> ${TMD}
@@ -55,89 +55,89 @@ lsbinfo()
 
 packageinfo()
 {
-	echo -e "## Package Information" >> ${TMD}
-	if [ "$(whereis apt | grep -i /bin/apt | wc -l)" -eq "1" ]; then
-		echo -e "\`\`\`" >> ${TMD}
-		echo -e "Packagemanager:\tapt" >> ${TMD}
-		echo -ne "Packagecount:\t" >> ${TMD}
-		apt list --installed 2>/dev/null | wc -l >> ${TMD}
-		echo -e "\`\`\`" >> ${TMD}
-	fi
+    echo -e "## Package Information" >> ${TMD}
+    if [ "$(whereis apt | grep -i /bin/apt | wc -l)" -eq "1" ]; then
+        echo -e "\`\`\`" >> ${TMD}
+        echo -e "Packagemanager:\tapt" >> ${TMD}
+        echo -ne "Packagecount:\t" >> ${TMD}
+        apt list --installed 2>/dev/null | wc -l >> ${TMD}
+        echo -e "\`\`\`" >> ${TMD}
+    fi
 
     ### TODO
-	# arch / pacman
-	# pacman -Qq | wc -l
-	
+    # arch / pacman
+    # pacman -Qq | wc -l
+    
     ### TODO
-	# Fedora
-	# dnf list installed | wc -l
+    # Fedora
+    # dnf list installed | wc -l
 }
 
 programminfo()
 {
-	echo -e "## Programm Information" >> ${TMD}
-	echo -e "|program|state|" >> ${TMD}
-	echo -e "| ------------- |:-------------:|" >> ${TMD}
-	
-	search4pgm=pipewire
-	programmdetails
-	search4pgm=curl
-	programmdetails
-	search4pgm=wget
-	programmdetails
-	search4pgm=flatpak
-	programmdetails
-	search4pgm=snap
-	programmdetails
+    echo -e "## Programm Information" >> ${TMD}
+    echo -e "|program|state|" >> ${TMD}
+    echo -e "| ------------- |:-------------:|" >> ${TMD}
+    
+    search4pgm=pipewire
+    programmdetails
+    search4pgm=curl
+    programmdetails
+    search4pgm=wget
+    programmdetails
+    search4pgm=flatpak
+    programmdetails
+    search4pgm=snap
+    programmdetails
 
 }
 
 programmdetails()
 {
-	echo -ne "| ${search4pgm} |" >> ${TMD}
-	if [ "$(whereis ${search4pgm} | grep -i /bin/${search4pgm} | wc -l)" -eq "1" ]; then
-		echo -ne "**installed**" >> ${TMD}
-	else
-		echo -ne "missing" >> ${TMD}
-	fi
-	echo -e "|" >> ${TMD}
+    echo -ne "| ${search4pgm} |" >> ${TMD}
+    if [ "$(whereis ${search4pgm} | grep -i /bin/${search4pgm} | wc -l)" -eq "1" ]; then
+        echo -ne "**installed**" >> ${TMD}
+    else
+        echo -ne "missing" >> ${TMD}
+    fi
+    echo -e "|" >> ${TMD}
 }
 
 listflatpak()
 {
-	if [ "$(whereis flatpak | grep -i /bin/flatpak | wc -l)" -eq "1" ]; then
-		echo -e "## Flatpak installed packages" >> ${TMD}
+    if [ "$(whereis flatpak | grep -i /bin/flatpak | wc -l)" -eq "1" ]; then
+        echo -e "## Flatpak installed packages" >> ${TMD}
         echo -e "|Name|Anwendungskennung|Version|Zweig|Ursprung|Installation|" >> ${TMD}
         echo -e "| ------------- | ------------- | ------------- | ------------- | ------------- | ------------- |" >> ${TMD}
-		flatpak list | sed 's/./|&/' | sed 's/\t/|/g' | sed 's/$/|/' >> ${TMD}
-	fi
+        flatpak list | sed 's/./|&/' | sed 's/\t/|/g' | sed 's/$/|/' >> ${TMD}
+    fi
 }
 
 listsnap()
 {
-	if [ "$(whereis snap | grep -i /bin/snap | wc -l)" -eq "1" ]; then
-		echo -e "## Snap installed packages" >> ${TMD}
-		echo -e "\`\`\`" >> ${TMD}
-		snap list >> ${TMD}
-		echo -e "\`\`\`" >> ${TMD}
-	fi
+    if [ "$(whereis snap | grep -i /bin/snap | wc -l)" -eq "1" ]; then
+        echo -e "## Snap installed packages" >> ${TMD}
+        echo -e "\`\`\`" >> ${TMD}
+        snap list >> ${TMD}
+        echo -e "\`\`\`" >> ${TMD}
+    fi
 }
 
 enviroment_vars()
 {
-	echo -e "## Enviroment" >> ${TMD}
-	echo -e "|varialbe            | value               |" >> ${TMD}
-	echo -e "| ------------------ |:-------------------:|" >> ${TMD}
-	echo -e "| \$XDG_SESSION_TYPE | ${XDG_SESSION_TYPE} |" >> ${TMD}
-	echo -e "| \$SHELL            | ${SHELL}            |" >> ${TMD}
-	echo -e "\n" >> ${TMD}
-	echo -e "<details>" >> ${TMD}
-	echo -e "  <summary>Show full enviroment variable list</summary>" >> ${TMD}
-	echo -ne "\n" >> ${TMD}
-	echo -e "\`\`\`" >> ${TMD}
-	printenv | sort >> ${TMD}
-	echo -e "\`\`\`" >> ${TMD}
-	echo -e "</details>\n" >> ${TMD}
+    echo -e "## Enviroment" >> ${TMD}
+    echo -e "|varialbe            | value               |" >> ${TMD}
+    echo -e "| ------------------ |:-------------------:|" >> ${TMD}
+    echo -e "| \$XDG_SESSION_TYPE | ${XDG_SESSION_TYPE} |" >> ${TMD}
+    echo -e "| \$SHELL            | ${SHELL}            |" >> ${TMD}
+    echo -e "\n" >> ${TMD}
+    echo -e "<details>" >> ${TMD}
+    echo -e "  <summary>Show full enviroment variable list</summary>" >> ${TMD}
+    echo -ne "\n" >> ${TMD}
+    echo -e "\`\`\`" >> ${TMD}
+    printenv | sort >> ${TMD}
+    echo -e "\`\`\`" >> ${TMD}
+    echo -e "</details>\n" >> ${TMD}
 }
 
 filesystem_info()
